315744e493a690fcb3ffd7312a6f23f57c429d9e
ViacheslavPiskun <vyacheslav2905@gmail.com>
2024-06-12 14:58:25 -0600

    draw on mouse move with brake on click away

--- client/src/components/Canvas/Canvas.jsx
+++ client/src/components/Canvas/Canvas.jsx
@@ -1,54 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import {useClickAway} from "react-use";
+import {useCallback, useEffect, useRef, useState} from "react";
+
+import shapeConstructors from "../../classes";
+
 import styles from './Canvas.module.css';
-
-class Shape {
-	constructor(start) {
-		this.start = start;
-		this.end = start;
-	}
-
-	setEnd(end) {
-		this.end = end;
-	}
-
-	draw(ctx) {}
-}
-
-class Line extends Shape {
-	draw(ctx) {
-		ctx.beginPath();
-		ctx.moveTo(this.start.x, this.start.y);
-		ctx.lineTo(this.end.x, this.end.y);
-		ctx.stroke();
-	}
-}
-
-class Rectangle extends Shape {
-	draw(ctx) {
-		const width = this.end.x - this.start.x;
-		const height = this.end.y - this.start.y;
-		ctx.strokeRect(this.start.x, this.start.y, width, height);
-	}
-}
-
-class Oval extends Shape {
-	draw(ctx) {
-		const radiusX = (this.end.x - this.start.x) / 2;
-		const radiusY = (this.end.y - this.start.y) / 2;
-		const centerX = this.start.x + radiusX;
-		const centerY = this.start.y + radiusY;
-
-		ctx.beginPath();
-		ctx.ellipse(centerX, centerY, Math.abs(radiusX), Math.abs(radiusY), 0, 0, 2 * Math.PI);
-		ctx.stroke();
-	}
-}
-
-const shapeConstructors = {
-	line: Line,
-	rectangle: Rectangle,
-	oval: Oval,
-};
 
 const Canvas = ({
 	shapeType,
@@ -61,28 +16,18 @@ const Canvas = ({
 	const [shapes, setShapes] = useState([]);
 	const canvasRef = useRef(null);
 
-	console.log(shapes)
-
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		setCtx(canvas.getContext('2d'));
 	}, []);
 
-	const handleCanvasClick = (e) => {
+	const handleMouseDown = (e) => {
 		const rect = canvasRef.current.getBoundingClientRect();
 		const clickPoint = { x: e.clientX - rect.left, y: e.clientY - rect.top };
-
-		if (!isDrawing) {
-			const ShapeConstructor = shapeConstructors[shapeType];
-			const newShape = new ShapeConstructor(clickPoint);
-			setCurrentShape(newShape);
-			setIsDrawing(true);
-		} else {
-			setIsDrawing(false);
-			currentShape.setEnd(clickPoint);
-			setShapes([...shapes, currentShape]);
-			setCurrentShape(null);
-		}
+		const ShapeConstructor = shapeConstructors[shapeType];
+		const newShape = new ShapeConstructor(clickPoint);
+		setCurrentShape(newShape);
+		setIsDrawing(true);
 	};
 
 	const handleMouseMove = (e) => {
@@ -98,14 +43,25 @@ const Canvas = ({
 		}
 	};
 
+	const handleMouseUp = useCallback( () => {
+		if (!isDrawing) return;
+		setIsDrawing(false);
+		setShapes([...shapes, currentShape]);
+		setCurrentShape(null);
+	}, [isDrawing, currentShape]);
+
 	useEffect(() => {
-		if(clearingCanvas) {
+		if (clearingCanvas) {
 			setIsDrawing(false);
-			setShapes([])
+			setShapes([]);
 			ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 			setClearingCanvas(false);
 		}
 	}, [ctx, clearingCanvas]);
+
+	useClickAway(canvasRef,() => {
+		handleMouseUp();
+	}, ["mouseup"])
 
 	return (
 		<div className={styles.wrapper}>
@@ -114,8 +70,9 @@ const Canvas = ({
 				height={600}
 				ref={canvasRef}
 				className={styles.canvas}
-				onClick={handleCanvasClick}
+				onMouseDown={handleMouseDown}
 				onMouseMove={handleMouseMove}
+				onMouseUp={handleMouseUp}
 			/>
 		</div>
 	);
--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
-const WSServer = require('express-ws')(app);
+const expressWs = require('express-ws')(app);
 
 const port = process.env.PORT || 5000;
+let clients = [];
 
 app.ws('/', (ws, req) => {
 	console.log('connected to WS server');
-	ws.send("Connection is successful");
+	clients.push(ws);
+	ws.send(JSON.stringify({ type: 'info', message: 'Connection is successful' }));
+
 	ws.on('message', (msg) => {
 		const message = JSON.parse(msg);
-		ws.send(`message from server: ${message.userName} connected`)
-	})
+		console.log(`Received message: ${msg}`);
+
+		clients.forEach(client => {
+			if (client !== ws && client.readyState === client.OPEN) {
+				client.send(JSON.stringify(message));
+			}
+		});
+	});
+
+	ws.on('close', () => {
+		clients = clients.filter(client => client !== ws);
+		console.log('Connection closed');
+	});
 });
 
 app.listen(port, () => {
 	console.log(`server started on port ${port}`);
-})
+});
